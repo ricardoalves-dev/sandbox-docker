fcb6683988698c9cbaba5e4de67e6f1340a40295
ricardoalves-dev <ricardoalvesdev@hotmail.com>
2026-09-05 09:43:02 -0300

    feat(app): criar bootstrap da aplicação

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import "reflect-metadata";
 import express from 'express';
 import { uptime } from 'node:process';
+import { pgDataSource } from "./db/postgres-datasource.js";
 
 const app = express();
-const port = 3000;
 
 // Types
 const HttpStatus = {
@@ -22,11 +22,29 @@ app.get('/health', (_req, res, _next) => {
   });
 });
 
-app.listen(port, (err) => {
-  if (err) {
-    console.error(`An error occurred: ${err}`);
+// Bootstrap
+async function bootstrap() {
+  try {
+    const port = Number(process.env.API_PORT) ?? 0;
+    const dbPort = Number(process.env.DB_PORT) ?? 0;
+
+    await pgDataSource.initialize();
+    console.log(`Database is connected on port: ${dbPort}`);
+
+    await new Promise<void>((res, rej) => {
+      app.listen(port, (err) => {
+        if (err) rej(err);
+        res();
+      });
+    });
+
+    console.log(`Application in running on port: ${port}`);
+  } catch (error) {
+    console.log(`An error occurred: ${error}`);
+    if (pgDataSource.isInitialized) await pgDataSource.destroy();
     process.exit(1);
   }
+}
 
-  console.log(`Server is running on port ${port}`);
-});
+bootstrap();
+
